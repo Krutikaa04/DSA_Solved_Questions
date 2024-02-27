e7222a34e26b78db4a7cf30f7bdcb88dd5c390f6
Krutikaa04 <waghkrutika01@gmail.com>
2024-02-27 17:03:17 +0530

    file added

---
+++ SecondLargestElement.java
@@ -0,0 +1,32 @@
+//print the second largest element in an array
+
+class Solution {
+    int print2largest(int arr[], int n) {
+        // code here
+        int secondlarge;
+        if(n < 2){
+            return -1;
+        }
+        
+        int large = secondlarge = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < n; i++)
+    {
+        large = Math.max(large, arr[i]);
+    }
+ 
+    
+    for(int i = 0; i < n; i++)
+    {
+        if (arr[i] != large)
+            secondlarge = Math.max(secondlarge, arr[i]);
+    }
+    if (secondlarge == Integer.MIN_VALUE){
+        return -1;
+    }
+    else{
+        return secondlarge;
+    }
+    }
+}
+
