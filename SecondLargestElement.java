6af5116745057331c383fce6c9235845747bacf0
Krutikaa04 <waghkrutika01@gmail.com>
2024-03-01 22:34:27 +0530

    file updated

--- SecondLargestElement.java
+++ SecondLargestElement.java
@@ -1,5 +1,40 @@
 //print the second largest element in an array
 
+//best optimal method with time complexity O(N)
+
+class Solution {
+    int print2largest(int arr[], int n) {
+        // code here
+        int largest = arr[0];
+        int secondlargest = -1;
+        if(n < 2){
+            return -1;
+        }
+        for(int i = 1; i< n; i++){
+            if(arr[i] > largest){
+                secondlargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > secondlargest){
+                secondlargest = arr[i];
+            }
+        }
+        return secondlargest;
+    }
+}
+
+//explanation of the optimal code
+
+// consider the first element of array as largest and while initializing give -1 to secondlargest as the minimum value.
+// as first element is assigned to largest loop will run from second element to end of array. now if the second element is greater than the first
+// element which is assigned to largest the second element becomes largest and first element becomes secondlargest.
+// the loop goes on. now if a condition arises where in if the element is smaller than the largest but is greater than the secondlargest,
+// the element is assigned to secondlargest. in the end we return the secondlargest value.
+
+// the time complexity is O(N) as the loop is run only once through the array of N elements and so is the optimal solution
+
+
+//method 2
 class Solution {
     int print2largest(int arr[], int n) {
         // code here
@@ -31,7 +66,7 @@ class Solution {
 }
 
 
-// method 2   by slicing
+// method 3   by slicing
 
 class Solution {
     int print2largest(int arr[], int n) {
