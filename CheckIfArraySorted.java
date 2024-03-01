15b8c057975a980d74eca9e38309ad5a7d2d0d43
Krutikaa04 <waghkrutika01@gmail.com>
2024-03-01 23:18:56 +0530

    file updated

---
+++ CheckIfArraySorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    boolean arraySortedOrNot(int[] arr, int n) {
+        // code here
+        if(n == 0 || n == 1){
+            return true;
+        }
+            for(int i=1;i<n;i++){
+            if(arr[i]<arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
