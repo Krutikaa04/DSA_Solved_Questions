7c170303b443a7899e60b4fa263dcb8dad179b3f
Krutikaa04 <waghkrutika01@gmail.com>
2024-03-03 23:09:30 +0530

    files updated

--- SearchElementInArray.java
+++ SearchElementInArray.java
@@ -23,3 +23,24 @@ class Solution{
 
 
 //as the loop is run only once for whole array time complexity is O(N)
+
+
+
+//search element in sorted array 
+
+class Solution{
+    static int searchInSorted(int arr[], int N, int K)
+    {
+        
+        // Your code here
+        if(N<1){
+            return -1;
+        }
+        for(int i=0; i<N; i++){
+            if(arr[i] == K){
+                return 1;
+            }
+        }
+        return -1;
+    }
+}
